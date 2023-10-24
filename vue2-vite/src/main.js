53ee4401976345830e287211f307ebbf1bfb00e3
chzky <hylk453030@gmail.com>
2023-10-24 14:46:51 +0800

    +

--- vue2-vite/src/main.js
+++ vue2-vite/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './assets/scss/commonStyle/mod.scss'
+import './assets/commonStyle/mod.scss'
 new Vue({
   el: '#app',
   render: h => h(App),
